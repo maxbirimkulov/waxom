c2bc9b596348d157d519418bc9ecb66d52aaf7e9
Igor Samartsev <igorsamartsev@gmail.com>
2020-03-24 21:03:30 +0600

    initial

---
+++ common.js
@@ -0,0 +1,45 @@
+$( document ).ready(function() {
+    $('.slider').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+    
+    $('.post-content').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+    
+    $('.burger').on('click',function(){
+        $('.menu').addClass('active');
+    });
+    $('.close').on('click',function(){
+        $('.menu').removeClass('active');
+    });
+    $('.popup').magnificPopup();
+});
